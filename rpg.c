170cf2fb835b34d704981e3a7dcfe5bf6a38121e
Armand Alvarez <minimcloving@users.noreply.github.com>
2016-08-11 18:14:52 -0400

    Add files via upload
    Changes:
    >Created setUpGame() function. This sets up the games defualt settings.
    >Updated setupScreenSize() function.
    >Updated printLaunchScreen() function.
    >Updated printHeader() function

--- rpg.c
+++ rpg.c
@@ -1,9 +1,37 @@
+/*
+Armand Alvarez & Luke Terhall
+Project Started 8/11/2016
+
+KnightRPG.c
+*/
+
+
+
+/*			CURRENTLY IN PROGRESS			*/
+/*PUT ANY FUNCTIONS THAT ARE NOT FINISHED HERE*/
+/*--------------------------------------------*/
+void mainMenu(void);
+void setUpGame(void);
+/*--------------------------------------------*/
+
+
+/*			Function Prototypes			*/
+/*	ONLY PUT FINISHED FUNCTIONS HERE	*/
+/*--------------------------------------*/
+void setupScreenSize(void);
+void printLaunchScreen(void);
+void printHeader(int style);
+/*--------------------------------------*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <string.h>
 #include <math.h>
 #include <time.h>
+#include <unistd.h>
 
 
 /*			FILE NAMES			*/
@@ -11,38 +39,43 @@
 #define LAUNCHTEXT "LaunchTitle.txt"
 /*------------------------------*/
 
+#define MAXNAMELENGTH 12
+
 typedef struct Player
 {
 	int xPosition;
 	int yPosition;
 	int health;
+	char *name;
 }Player;
 
-/*			Function Prototypes			*/
-/*--------------------------------------*/
-void setupScreenSize(void);
-void printLaunchScreen(void);
-/*--------------------------------------*/
-
+Player PLAYER;
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 int main (void)
 {	
-	setupScreenSize();
-	system("cls");
+	setUpGame();
 	
 	printLaunchScreen();
 
 	/*TEST FUNCTION*/
-	
+	printHeader(2);
 	/*END TEST FUNCTION*/
 }
 
+
+/* Sets up the game settings to default. */
+void setUpGame(void)
+{
+	setupScreenSize();
+	PLAYER.name="";
+}
 
 
 /* Sets up the dimensions for CMD prompt. */
 void setupScreenSize(void)
 {
-  	system("MODE 135, 40");
+  	system("MODE 105, 25");
 	system("cls");
 }
 
@@ -56,10 +89,53 @@ void printLaunchScreen(void)
 	if (fpt)
 	{
 		while ((c=getc(fpt)) != EOF)
+		{
 			putchar(c);
+			usleep(6000);
+		}
 	fclose(fpt);
+	getch();
 	}
 }
 
+
+
+/* 	Opens Main Menu. 
+	Includes Start Game.
+	Includes Credits. */
+void mainMenu(void)
+{
+	printHeader(1);
+	
+	
+}
+
+
+/*	Clears the screen.
+	Prints the header on top.
+	1 for basic header (name of game only).
+	2 for full header (DEFINE FULL HEADER). */
+void printHeader(int style)
+{
+	system("cls");
+	
+	if (style==1)
+	{
+		printf("\t\t\t\t\t\tKNIGHT RPG\n");
+		printf("---------------------------------------------------------------------------------------------------------\n");
+		getch();
+	}
+	
+	else if (style==2)
+	{
+		/* Use this failsafe if the character name has not been created yet. */
+		if(PLAYER.name=="")
+		{
+			printf("\t\t\t\t\t\tKNIGHT RPG\n");
+			printf("---------------------------------------------------------------------------------------------------------\n");
+			getch();
+		}
+	}
+}
 
 
